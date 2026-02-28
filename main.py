1ca204eb01e8885f192f8b4caa107d836bad42e2
Junaidaliniz <157386475+Junaidaliniz@users.noreply.github.com>
2026-02-28 23:50:52 +0500

    Update and rename bot_optimized (2) (4).py to main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from datetime import datetime, timedelta
 from telegram.ext import Application, CommandHandler, ContextTypes, CallbackQueryHandler, MessageHandler, filters
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, InputFile
 
-YOUR_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "7674691123:AAEMKKzUSxkJQK0Obv0ptIRBixYEui_6y78")
+YOUR_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "8225744822:AAEDhMi-9u2GlgZstgVPBzai_sXCCFAyb14")
 
 DATA_DIR = "data"
 PANELS_FILE = os.path.join(DATA_DIR, "panels.json")
